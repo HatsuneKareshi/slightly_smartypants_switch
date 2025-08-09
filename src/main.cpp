5175a21fd2779a38ac6012df148aff0744177ccc
DreamingShynnevilla <pmtriet23@clc.fitus.edu.vn>
2025-08-09 13:53:19 +0700

    testing creating Json

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <mqtt.h>
 #include <wf.h>
+#include <ArduinoJson.h>
+#include <temp_humid_sensor.h>
+#include <rtc.h>
+#include <light_sensor.h>
+#include <motion_sensor.h>
 
 String ssid = "j4_big_brotha";
 String password = "wokfuckboi";
 String host = "broker.hivemq.com";
 int port = 1883;
 
+// Initialize sensors
 mqttbroker brokeass(host, port);
+temp_humid_sensor DHTSensor;
+light_sensor lightSensor;
+motion_sensor motionSensor;
+
+// Init time
+unsigned long lastPublish = 0;
 
 void setup()
 {
@@ -26,6 +38,20 @@ void loop()
   }
   brokeass.loop();
   Serial.println(brokeass.get_message("/broken/ass"));
-  brokeass.publish("MSSV/temperature", "TIEU");
+  // Create a JSON document
+  if (millis() - lastPublish >= 10000) 
+  {
+    StaticJsonDocument<200> doc;  
+    doc["temperature"] = DHTSensor.get_temp();
+    doc["humidity"] = DHTSensor.get_humidity();
+    doc["light"] = lightSensor.get_light_analog();
+    doc["motion"] = motionSensor.get_motion();
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+
+    brokeass.publish("MSSV/temperature", jsonString.c_str());
+    lastPublish = millis();
+  }
   delay(1000);
 }
